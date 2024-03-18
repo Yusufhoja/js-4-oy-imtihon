e299d8646381adc28ea02749283343f7a6b7c344
Yusufhoja <usufhozah@gmail.com>
2024-03-18 23:22:53 +0500

    js 4-oy imtihon

---
+++ script.js
@@ -0,0 +1,63 @@
+//1
+// let a=5%2
+// alert(a)
+// console.log(Math.floor(a));
+
+//2
+// let c= Math.round(Math.random()*9+1)
+// alert(c)
+
+//3
+// let d=12.510
+// let e=Math.round(d)
+// alert(e)
+
+//4
+// let g="Mars IT School"
+// let h=g.length
+// alert(h)
+
+//5
+// function printMars() {
+//     for (let i = 0; i < 10; i++) {
+//         console.log("MARS IT SCHOOL");
+//     }
+// }
+
+// printMars();
+
+
+
+//6
+// let harflar=['a','b','c','d','e','f','j','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+// let ismim=harflar[24]+harflar[20]+harflar[18]+harflar[20]+harflar[5]+harflar[7]+harflar[14]+harflar[6]+harflar[0];
+// console.log(ismim);
+
+//7
+// let k=+prompt("yoshingizni kiriting")
+// if(k<18){
+//     alert("siz balogat yoshiga yetmagansiz")
+// }
+// else{
+//     alert("siz balogat yoshiga yetkansiz")
+// }
+
+
+// 8
+// const myName = prompt('ismizni kiriting');
+// const reversedName = myName.split('').reverse().join('');
+// console.log(reversedName);
+
+//9
+//Biza Swaitch case ni otmaganmiz
+
+// 10
+
+// let number=[1,2,3,4,5,6,7,8,9,10,];
+// let pustoy=[];
+// for(let i=0; i<number.length; i++){
+//     if(number[i]%2==0)
+//       pustoy.push (number[i]);
+//   }
+//   console.log(pustoy);
+
